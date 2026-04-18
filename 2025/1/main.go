5ce1d536cabedaa939683e10a4781be7643052b1
spolisar <22416070+spolisar@users.noreply.github.com>
2026-04-17 20:02:40 -0400

    solved 2025/day 1

--- 2025/1/main.go
+++ 2025/1/main.go
@@ -1,22 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func RotateCount(input string) int {
 	// initial position is 50
 	position := 50
-	return 0
+	count := 0
+	numSlots := 100
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	for scanner.Scan() {
+		// fmt.Println(scanner.Text())
+		text := scanner.Text()
+		direction := text[0]
+		distance, err := strconv.Atoi(text[1:])
+		if err != nil {
+			return -1
+		}
+		// fmt.Printf("%c %d\n", direction, distance)
+		switch direction {
+		case 'L':
+			position = (position - distance + numSlots) % numSlots
+			// if position < 0 {
+			// 	position = numSlots + position
+			// }
+		case 'R':
+			position = (position + distance) % numSlots
+		}
+		if position == 0 {
+			count++
+		}
+		// fmt.Printf("position: %d\n", position)
+	}
+	return count
 }
 
 func main() {
-	exampleFilename := "example.txt"
-	b, err := os.ReadFile(exampleFilename)
+	// exampleFilename := "example.txt"
+	inputFilename := "input.txt"
+	b, err := os.ReadFile(inputFilename)
 	if err != nil {
 		fmt.Println(err)
 	}
 	inputStr := string(b)
-	fmt.Println(inputStr)
+	// fmt.Println(inputStr)
+	zeroCount := RotateCount(inputStr)
+	fmt.Printf("zero count: %d", zeroCount)
 }
--- 2025/1/rotations_test.go
+++ 2025/1/rotations_test.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
 func TestExample(t *testing.T) {
-	// exampleFilename := "example.txt"
+	inputFilename := "example.txt"
 
-	result := RotateCount("")
+	b, err := os.ReadFile(inputFilename)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	inputStr := string(b)
+	result := RotateCount(inputStr)
 	expected := 3
 	if result != expected {
 		t.Errorf("Result was incorrect, got: %d, want: %d.", result, 3)
